5381ce5d9aefe1e77911917ec17d2a0a97283afd
sure <2997605195@qq.com>
2026-03-14 01:07:51 +0800

    feat-admin框架搭建（HTML部分）

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -3,7 +3,42 @@
 </script>
 
 <template>
-<div>admin</div>
+  <div class="s_admin">
+    <div class="s_aside">
+      <div class="s_logo">
+
+      </div>
+      <div class="s_menu">
+
+      </div>
+    </div>
+    <div class="s_main">
+      <div class="s_header">
+        <div class="s_breadcrumbs">
+
+        </div>
+        <div class="s_actions">
+          <icon-home />
+          <icon-sun-fill />
+          <icon-moon-fill />
+          <icon-fullscreen />
+          <icon-fullscreen-exit />
+          <div class="s_user_info_action">
+
+          </div>
+
+        </div>
+      </div>
+      <div class="s_tabs">
+
+      </div>
+      <div class="s_container">
+        <RouterView></RouterView>
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
